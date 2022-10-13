d98a92d94da01f35f621fc2c0866a53a7aa36577
Rohith Iyengar <67773707+rohithj7@users.noreply.github.com>
2022-10-13 08:12:09 +0530

    Add files via upload

---
+++ src/Config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DEFAULT_PEOPLE_LIMIT 5
+#define MOUNTED_INSIDE
+
+/***   WiFi   ***/
+//#define AP_MODE Does not work reliably
+#define STASSID "PeopleCounter"
+#define STAPSK  "Password"
+
+/***   Debugging Mode   ***/
+//#define DEBUG_COUNTER
+
+#endif
